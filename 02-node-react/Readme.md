40c5e50b6dc88e207af2fbea9b3b741c45660f66
Zoila Villatoro <zoilavillatoro6694@gmail.com>
2020-08-10 13:36:05 -0600

    added signup

--- 02-node-react/Readme.md
+++ 02-node-react/Readme.md
@@ -1,1 +1,11 @@
 # Node React
+
+Comands
+
+```
+    yarn start
+```
+
+Packages use
+
+1. [React-router-dom](https://yarnpkg.com/package/react-router-dom)
--- 02-node-react/react-front/src/user/Signup.jsx
+++ 02-node-react/react-front/src/user/Signup.jsx
@@ -13,15 +13,14 @@ class Signup extends Component {
     };
   }
 
-  componentWillMount() {}
-
   // Another way
   handleChange = (name) => (event) => {
-    //this.setState({ error: "" });
+    this.setState({ error: "" });
     this.setState({ [name]: event.target.value });
   };
 
   handleChangeInput = (e) => {
+    this.setState({ error: "" });
     this.setState({
       [e.target.name]: e.target.value,
     });
@@ -36,7 +35,23 @@ class Signup extends Component {
       password,
     };
     //console.log("user", user);
-    fetch("http://localhost:4000/api/auth/signup", {
+    this.signup(user).then((data) => {
+      if (data.err) {
+        this.setState({ error: data.err });
+      } else {
+        this.setState({
+          error: "",
+          name: "",
+          email: "",
+          password: "",
+          open: true,
+        });
+      }
+    });
+  };
+
+  signup = (user) => {
+    return fetch("http://localhost:4000/api/auth/signup", {
       method: "POST",
       headers: {
         Accept: "application/json",
@@ -44,56 +59,75 @@ class Signup extends Component {
       },
       body: JSON.stringify(user),
     })
-      .then((response) => response.json())
+      .then((response) => {
+        return response.json();
+      })
       .catch((err) => console.log(err));
   };
 
+  signupForm = (name, email, password) => {
+    return (
+      <form>
+        <div className="form-group">
+          <label className="text-muted">Name</label>
+          <input
+            //onChange={this.handleChange("name")}
+            onChange={this.handleChangeInput}
+            type="text"
+            className="form-control"
+            value={name}
+            name="name"
+          />
+        </div>
+        <div className="form-group">
+          <label className="text-muted">Email</label>
+          <input
+            //onChange={this.handleChange("email")}
+            onChange={this.handleChangeInput}
+            type="email"
+            className="form-control"
+            value={email}
+            name="email"
+          />
+        </div>
+        <div className="form-group">
+          <label className="text-muted">Password</label>
+          <input
+            //onChange={this.handleChange("password")}
+            onChange={this.handleChangeInput}
+            type="password"
+            className="form-control"
+            value={password}
+            name="password"
+          />
+        </div>
+        <button
+          onClick={this.clickSubmit}
+          className="btn btn-raised btn-primary"
+        >
+          Submit
+        </button>
+      </form>
+    );
+  };
   render() {
-    const { name, email, password } = this.state;
+    const { name, email, password, error, open } = this.state;
     return (
       <div className="container">
         <h2 className="mt-5 mb-5">Signup</h2>
-        <form>
-          <div className="form-group">
-            <label className="text-muted">Name</label>
-            <input
-              //onChange={this.handleChange("name")}
-              onChange={this.handleChangeInput}
-              type="text"
-              className="form-control"
-              value={name}
-              name="name"
-            />
-          </div>
-          <div className="form-group">
-            <label className="text-muted">Email</label>
-            <input
-              //onChange={this.handleChange("email")}
-              onChange={this.handleChangeInput}
-              type="email"
-              className="form-control"
-              value={email}
-              name="email"
-            />
-          </div>
-          <div className="form-group">
-            <label className="text-muted">Password</label>
-            <input
-              //onChange={this.handleChange("password")}
-              onChange={this.handleChangeInput}
-              type="password"
-              className="form-control"
-              value={password}
-              name="password"
-            />
-          </div>
-          <button
-            onClick={this.clickSubmit}
-            className="btn btn-raised btn-primary"
-          >
-            Submit
-          </button>
-        </form>
+        <div
+          className="alert alert-danger"
+          style={{ display: error ? "" : "none" }}
+        >
+          {error}
+        </div>
+        <div
+          className="alert alert-info"
+          style={{ display: open ? "" : "none" }}
+        >
+          New account is successfully created, Please Sign In
+        </div>
+        {this.signupForm(name, email, password)}
       </div>
     );
   }
